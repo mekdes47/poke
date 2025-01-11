79d766bf1676f07e3587f96108ed1a2adfc5d8e3
Fitsum Sisay <fs64405@gmail.com>
2025-01-11 00:30:12 -0800

    initial commit

---
+++ Models/Pokemon.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pokedex.Models
+{
+    public class Pokemon
+    {
+        public string? Id {get; set;}
+        public string? Name {get; set;}  
+        public string? Type {get;set;}
+        public string? Ability {get;set;}
+        public int? Level {get;set;}
+        
+    }
+    
+}
